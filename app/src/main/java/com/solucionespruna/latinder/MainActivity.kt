3131ad66aebefbb7151ab0c02cb2d53f7147eb64
David Gonzalez <david.g@verypossible.com>
2024-05-11 11:26:10 -0500

    Add two cards, one on top of the other

--- app/src/main/java/com/solucionespruna/latinder/MainActivity.kt
+++ app/src/main/java/com/solucionespruna/latinder/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.solucionespruna.latinder.ui.card.CardScreen
 import com.solucionespruna.latinder.ui.theme.LaTinderTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +17,9 @@ class MainActivity : ComponentActivity() {
       LaTinderTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          CardScreen()
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  LaTinderTheme {
-    Greeting("Android")
   }
 }
---
+++ app/src/main/java/com/solucionespruna/latinder/ui/card/Card.kt
@@ -0,0 +1,91 @@
+package com.solucionespruna.latinder.ui.card
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.solucionespruna.latinder.R
+import com.solucionespruna.latinder.ui.theme.LaTinderTheme
+
+@Composable
+fun CardScreen() {
+  Box(Modifier.background(Color.White).padding(8.dp)) {
+    Card(text = "This is the bottom card")
+    Card(text = "This is the upper card")
+  }
+}
+
+@Composable
+@Preview(
+  showBackground = true,
+  showSystemUi = true,
+  uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
+fun CardScreenPreview() {
+    LaTinderTheme {
+        Surface {
+        CardScreen()
+        }
+    }
+}
+
+@Composable
+fun Card(text: String) {
+  Column(Modifier
+      .fillMaxSize()
+      .background(MaterialTheme.colorScheme.background)
+      .padding(8.dp)) {
+    Text(text = text,
+      Modifier
+        .fillMaxWidth()
+        .padding(8.dp), textAlign = TextAlign.Center)
+    Image(
+      painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "Card Image",
+      Modifier
+        .weight(1f)
+        .align(alignment = androidx.compose.ui.Alignment.CenterHorizontally))
+    Row (
+      Modifier
+        .fillMaxWidth()
+        .padding(8.dp),
+      horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+      Button(onClick = {}, Modifier.weight(1f)) {
+        Text(text = "Like")
+      }
+      Button(onClick = {}, Modifier.weight(1f)) {
+        Text(text = "Dislike")
+      }
+      Button(onClick = {}, Modifier.weight(1f)) {
+        Text(text = "Super Like")
+      }
+    }
+  }
+}
+
+@Composable
+@Preview(
+  showBackground = true,
+  showSystemUi = true,
+  uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
+fun CardPreview() {
+  LaTinderTheme {
+    Surface {
+      Card("Test Text")
+    }
+  }
+}
